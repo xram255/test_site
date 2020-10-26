345f6a49e4a271e5a525db90dd3db7c6d1618912
RoshanCS <roshan.cs790@gmail.com>
2020-10-26 21:46:53 +0530

    Create Table
    mysqli create table

--- connection_file.php
+++ connection_file.php
@@ -2,15 +2,37 @@
 	echo "initiating <br>";
 	$server = "localhost";
 	$user = "root";
-	$password = "fre";
-	$connection = mysqli_connect($server, $user, $password);
+	$password = "";
+	$database = "test";
+	//Connection
+	$connection = mysqli_connect($server, $user, $password, $database);
 	if (!$connection) 
 	{
 		die("<h4>Failed...!</h4> ".mysqli_connect_error());
 	}
 	else
 	{
-		echo "connection successful";
+		echo "connection successful <br>";
 	}
+	//SQL Command
+	$sql_command = "CREATE TABLE people 
+	(
+		id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+		name VARCHAR(30) NOT NULL,
+		lastname VARCHAR(30) NOT NULL,
+		telephone INT,
+		email VARCHAR(100), 
+		addres VARCHAR(100),
+		meta TIMESTAMP
+	)";
 
+	//Check SQL commands
+	if (mysqli_query($connection, $sql_command)) 
+	{
+		echo "SQL Command successful";
+	}
+	else
+	{
+		echo "sql error ".mysqli_error($connection);
+	}
  ?>
