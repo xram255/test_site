5de776a9d678630226e115b05c51eaad3aec6298
RoshanCS <roshan.cs790@gmail.com>
2021-03-16 15:12:39 +0530

    Create server_info.php

---
+++ server_info.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>This is the Title of the Document..</h1>
+    Your pet breed is: <?php echo $_POST["breed"]; ?><br>
+    Color is: <?php echo $_POST["color"]; ?>
+</body>
+</html>
